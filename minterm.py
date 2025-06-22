85330c63de4b974e122dbcd6bcca4056a83cfb34
carlos <carlos.j.bravo@hotmail.com>
2025-06-21 20:54:22 -0500

    refactor: Enhance Minterm class with Hamming distance, implicant creation, and method reduction
    The `Minterm` class has been modified to:
    
    - **Estimate Hamming distance**: This allows for a more effective and intelligent
      comparison of minterms, specifically to identify if there is more than a
      single bit change between them.
    - **Create an implicant minterm**: A new functionality has been added to
      create an implicant minterm directly from two existing minterms.
    - **Reduce unnecessary methods**: Several redundant or unnecessary methods
      have been removed from the class.
    
    These changes significantly improve the `Minterm` class by making its
    comparison logic more robust and efficient through Hamming distance
    estimation. The ability to create implicant minterms directly streamlines
    operations related to boolean algebra simplification. Furthermore, the
    reduction of unnecessary methods cleans up the codebase, enhancing
    maintainability and clarity.

--- minterm.py
+++ minterm.py
@@ -1,22 +1,42 @@
-from typing import Union
+from typing import Set
 
 
 class Minterm:
-    def __init__(self, minterm: str, ones: set):
-        self.minterm = minterm
-        self.ones = ones
+    def __init__(self, minterm: str, ones: Set):
+        self._minterm = minterm
+        self._ones = ones
+
+    @property
+    def minterm(self):
+        return self._minterm
+
+    @property
+    def ones(self):
+        return self._ones
 
     def __hash__(self):
-        return hash(self.minterm)
+        return hash(self._minterm)
 
-    def __eq__(self, other: Union[str, 'Minterm']):
-        if isinstance(other, Minterm):
-            return self.minterm == other.minterm
-        if isinstance(other, str):
-            return self.minterm == other
+    def hamming_distance(self, other: 'Minterm'):
+        return sum(
+            char1 != char2
+            for char1, char2 in zip(self._minterm, other._minterm)
+        )
+
+    def create_implicant(self, other:'Minterm'):
+        return Minterm(
+            "".join(
+                char1 if char1 == char2 else "-"
+                for char1, char2 in zip(self._minterm, other._minterm)
+            ),
+            {*self._ones, *other._ones}
+        )
+
+    def __eq__(self, other: 'Minterm'):
+        return self._minterm == other._minterm
 
     def __str__(self):
-        return self.minterm
+        return f"{self._minterm}"
 
     def __repr__(self):
-        return self.minterm
+        return f"{self._minterm}"
